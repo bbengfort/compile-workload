d5357a3c57f3d0813deb4a6cea073730bffabe8f
Benjamin Bengfort <benjamin@bengfort.com>
2017-01-25 07:17:04 -0500

    uncomment benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -15,12 +15,12 @@ WORKLOAD="./workload.py"
 
 # Run the standard file system tests
 mkdir $TESTDIR
-# $WORKLOAD -p redis -H -o $RESULTS $TESTDIR
-# $WORKLOAD -p postgres -o $RESULTS $TESTDIR
-# $WORKLOAD -p nginx -o $RESULTS $TESTDIR
-# $WORKLOAD -p apache -o $RESULTS $TESTDIR
-# $WORKLOAD -p ruby -o $RESULTS $TESTDIR
-# $WORKLOAD -p python -o $RESULTS $TESTDIR
+$WORKLOAD -p redis -H -o $RESULTS $TESTDIR
+$WORKLOAD -p postgres -o $RESULTS $TESTDIR
+$WORKLOAD -p nginx -o $RESULTS $TESTDIR
+$WORKLOAD -p apache -o $RESULTS $TESTDIR
+$WORKLOAD -p ruby -o $RESULTS $TESTDIR
+$WORKLOAD -p python -o $RESULTS $TESTDIR
 rmdir $TESTDIR
 
 # Run the MemFS tests
@@ -35,10 +35,10 @@ run_memfs_workload() {
 }
 
 run_memfs_workload redis
-# run_memfs_workload postgres
-# run_memfs_workload nginx
-# run_memfs_workload apache
-# run_memfs_workload ruby
-# run_memfs_workload python
+run_memfs_workload postgres
+run_memfs_workload nginx
+run_memfs_workload apache
+run_memfs_workload ruby
+run_memfs_workload python
 
 rmdir $MEMFSDIR
